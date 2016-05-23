86afd4ab6b77a12d07efe396c7cccb1fa206fd57
FelixCSM <felix-voelker@web.de>
2016-05-24 00:24:13 +0200

    make_sparseM is done and tested on N=5

--- src/make_sparseM.m
+++ src/make_sparseM.m
@@ -22,7 +22,30 @@
 ## Author:  <Felix Völker>
 ## Created: 2016-05-23
 
-function [retval] = make_sparseM (N)
+
+#@param N, amount of T matrices (ref exercise)
+#@return value is newly created required sparse matix
+function [A] = make_sparseM (N)
+
+    #actual size of A
     n = N**2;
+    #"allocating" possible entries
     A = sparse(n,n);
+    
+    #this for-loop writes the T's in A
+    for T = 0:N:n-1
+        for i = T+1:T+N-1
+            A(i,i)=4;
+            A(i,i+1)=A(i+1,i)=-1;
+        endfor
+        A(T+N,T+N)=4;
+    endfor
+    
+    #this for-loop writes the negative identity matrices into A
+    for i = 1:n-N
+        A(i,i+N)=A(i+N,i)=-1;
+     endfor
 endfunction
+
+
+##this function can be tested with spy(A) and full(A)
